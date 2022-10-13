b44feafb909be8090fe0feaa4ac4658909d1592d
Mitesh2001 <miteshladva.dgrs@gmail.com>
2022-10-13 14:17:54 +0530

    PHP Filters Advanced

---
+++ IPv6_address.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Variable to check
+$ip = "2001:0db8:85a3:08d3:1319:8a2e:0370:7334";
+
+if (filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_IPV6)) {
+      echo("$ip is a valid IPv6 address");
+} else {
+      echo("$ip is not a valid IPv6 address");
+}
+?>
+
+</body>
+</html>
